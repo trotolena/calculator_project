11f4a6520f1cebba9fa9441cb4fe90cd42fe48e4
Yuliia Plakhotna <y.plakhotna@gmail.com>
2022-11-22 18:00:27 +0100

    Changes in calc

--- calc.py
+++ calc.py
@@ -1,7 +1,10 @@
-# Створіть командно (2-3 учні) програму-калькулятор. Перший учень створює проект/файл/клас,
-# де буде знаходитися програма й надіслати його за допомогою інструментів IDE на віддалений сервер.
-# Кожний наступний учень повинен клонувати проект на свій комп’ютер, додати свої частинки програми,
-# зробити коміти й надіслати ці зміни на віддалений сервер.
+# Створіть командно (2-3 учні) програму-калькулятор. Перший учень
+# створює проект/файл/клас, де буде знаходитися програма й надіслати його
+# за допомогою інструментів IDE на віддалений сервер.
+# Кожний наступний учень повинен клонувати проект на свій комп’ютер,
+# додати свої частинки програми, зробити коміти й надіслати ці зміни
+# на віддалений сервер.
+
 
 def func_sum(x, y) -> int:
     return x + y
@@ -19,9 +22,9 @@ def func_multiplication(x, y) -> int:
     return x * y
 
 
-print("Welcome to HBVY calculator")
+print('Welcome to HBVY calculator')
 
-print("""Available Operations :
+print("""Available Operations:
 '+'
 '-'
 '*'
@@ -39,41 +42,40 @@ def is_number(obj):
 
 
 while True:
-    step = input('Choose the operation : ')
+    step = input('Choose the operation: ')
     match step:
         case '+':
-            number_1 = input('Enter 1st number : ')
-            number_2 = input('Enter 2nd number : ')
+            number_1 = input('Enter 1st number: ')
+            number_2 = input('Enter 2nd number: ')
             if is_number(number_1) and is_number(number_2):
-                print(f"Result is : {func_sum(float(number_1), float(number_2))}")
+                print(f'Result is: {func_sum(float(number_1), float(number_2))}')
             else:
-                print("Wrong input!")
+                print('Wrong input!')
         case '-':
-            number_1 = input('Enter 1st number : ')
-            number_2 = input('Enter 2nd number : ')
+            number_1 = input('Enter 1st number: ')
+            number_2 = input('Enter 2nd number: ')
             if is_number(number_1) and is_number(number_2):
-                print(f"Result is : {func_subtraction(float(number_1), float(number_2))}")
+                print(f'Result is: {func_subtraction(float(number_1), float(number_2))}')
             else:
-                print("Wrong input!")
+                print('Wrong input!')
         case '*':
-            number_1 = input('Enter 1st number : ')
-            number_2 = input('Enter 2nd number : ')
+            number_1 = input('Enter 1st number: ')
+            number_2 = input('Enter 2nd number: ')
             if is_number(number_1) and is_number(number_2):
-                print(f"Result is : {func_multiplication(float(number_1), float(number_2))}")
+                print(f'Result is: {func_multiplication(float(number_1), float(number_2))}')
             else:
-                print("Wrong input!")
+                print('Wrong input!')
         case '/':
-            number_1 = input('Enter 1st number : ')
-            number_2 = input('Enter 2nd number : ')
+            number_1 = input('Enter 1st number: ')
+            number_2 = input('Enter 2nd number: ')
             if number_2 == '0':
-                print("You can't devide by zero")
+                print('You can not divide by zero')
             elif is_number(number_1) and is_number(number_2):
-                print(f"Result is : {func_division(float(number_1), float(number_2))}")
+                print(f'Result is: {func_division(float(number_1), float(number_2))}')
             else:
-                print("Wrong input!")
+                print('Wrong input!')
         case 'exit':
-            print("\nBye bye :)")
+            print('Bye bye :)')
             break
         case other:
-            print("Wrong input!")
-
+            print('Wrong input!')
