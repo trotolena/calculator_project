b8495aae6d994d09495ec181da9fefc865b810cc
Vlad Vidan <kozlovvlad1994@gmail.com>
2022-11-22 18:01:52 +0200

    Added: basic algorithm of calculator

--- calc.py
+++ calc.py
@@ -17,3 +17,63 @@ def func_division(x, y) -> int:
 
 def func_multiplication(x, y) -> int:
     return x * y
+
+
+print("Welcome to HBVY calculator")
+
+print("""Available Operations :
+'+'
+'-'
+'*'
+'/'
+'exit'
+""")
+
+
+def is_number(obj):
+    try:
+        float(obj)
+        return True
+    except ValueError:
+        return False
+
+
+while True:
+    step = input('Choose the operation : ')
+    match step:
+        case '+':
+            number_1 = input('Enter 1st number : ')
+            number_2 = input('Enter 2nd number : ')
+            if is_number(number_1) and is_number(number_2):
+                print(f"Result is : {func_sum(float(number_1), float(number_2))}")
+            else:
+                print("Wrong input!")
+        case '-':
+            number_1 = input('Enter 1st number : ')
+            number_2 = input('Enter 2nd number : ')
+            if is_number(number_1) and is_number(number_2):
+                print(f"Result is : {func_subtraction(float(number_1), float(number_2))}")
+            else:
+                print("Wrong input!")
+        case '*':
+            number_1 = input('Enter 1st number : ')
+            number_2 = input('Enter 2nd number : ')
+            if is_number(number_1) and is_number(number_2):
+                print(f"Result is : {func_multiplication(float(number_1), float(number_2))}")
+            else:
+                print("Wrong input!")
+        case '/':
+            number_1 = input('Enter 1st number : ')
+            number_2 = input('Enter 2nd number : ')
+            if number_2 == '0':
+                print("You can't devide by zero")
+            elif is_number(number_1) and is_number(number_2):
+                print(f"Result is : {func_division(float(number_1), float(number_2))}")
+            else:
+                print("Wrong input!")
+        case 'exit':
+            print("\nBye bye :)")
+            break
+        case other:
+            print("Wrong input!")
+
